c1571c231ac7151d58e2640260d24f7f820d2098
Blake Davis <58578456+davisblakep@users.noreply.github.com>
2021-02-02 17:30:08 -0500

    Initial site creation

---
+++ earlgrey/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Earl Grey's Rescue</h1>
+      <p>Site coming soon!</p>
+    </div>
+  );
+}
+
+export default App;
