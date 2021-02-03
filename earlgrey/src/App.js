a0be19a77cf9e9750e2512eb31d01fded8596e69
Blake Davis <58578456+davisblakep@users.noreply.github.com>
2021-02-02 23:02:41 -0500

    Quick switch from h2 to p for description

--- earlgrey/src/App.js
+++ earlgrey/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <NavBar />
       <h1 style={{ marginLeft: "40px" }}>Earl Grey's Rescue</h1>
       <p style={{ marginLeft: "40px" }}>Full Site coming soon!</p>
-      <h2 style={{ marginLeft: "40px" }}>Earl Grey's Rescue is a site dedicated to support wild animals within the southwest region of Ohio.</h2>
+      <p style={{ marginLeft: "40px" }}>Earl Grey's Rescue is a site dedicated to support wild animals within the southwest region of Ohio.</p>
     </div>
   );
 }
