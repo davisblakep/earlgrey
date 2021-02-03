ea50f1ee3c2bca07438bb2507dd4b4b0e2c0d577
Blake Davis <58578456+davisblakep@users.noreply.github.com>
2021-02-02 23:23:17 -0500

    Changed animals to kitties

--- earlgrey/src/App.js
+++ earlgrey/src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1 style={{ marginLeft: "40px", marginRight: "40px" }}>Animal Rescue</h1>
+      <h1 style={{ marginLeft: "40px", marginRight: "40px" }}>Kitty Rescue</h1>
       <h3 style={{ marginLeft: "40px", marginRight: "40px" }}>Full Site coming soon!</h3>
       <br></br>
-      <p style={{ marginLeft: "40px", marginRight: "40px" }}>Earl Grey's Rescue is an organization dedicated to support wild animals within the southwestern region of Ohio.</p>
+      <p style={{ marginLeft: "40px", marginRight: "40px" }}>Earl Grey's Rescue is an organization dedicated to support wild and domesticated cats within the southwestern region of Ohio.</p>
     </div>
   );
 }
